bbab705696e83b3253580f6b350be0682e2e0a60
Sumit <sumit31raj@gmail.com>
2021-06-28 13:51:32 +0530

    Added bootstrap and interfaces fixed api

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import routes from '../routes';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
--- src/services/flickr/config.ts
+++ src/services/flickr/config.ts
@@ -1,9 +1,9 @@
 const baseUrl = 'https://www.flickr.com/services/rest/';
 const apiKey = process.env.REACT_APP_FLICKR_API_KEY;
 
-export const getURL = (params: {[key: string]: string }) => {
+export const getURL = (params: { [key: string]: string }) => {
   const searchParams = new URLSearchParams(params);
 
-  return `${baseUrl}?api_key=${apiKey}&format=json&nojsoncallback=true&${searchParams.toString()}`;
+  return `${baseUrl}?api_key=${apiKey}&format=json&nojsoncallback=true&${searchParams.toString()}&tags=animals,nature,weather,attractions,places,design,sealife,football,sports&extras=description,url_s,tags,owner_name`;
 }
 
